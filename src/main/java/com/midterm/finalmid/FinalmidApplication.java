515f665e1ff854a9cfa196e72a999d4eda980e53
ulkiller <ulkersirinov@gmail.com>
2024-10-25 12:36:47 +0400

    salam

---
+++ src/main/java/com/midterm/finalmid/FinalmidApplication.java
@@ -0,0 +1,13 @@
+package com.midterm.finalmid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalmidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalmidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/midterm/finalmid/FinalmidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm.finalmid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalmidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
